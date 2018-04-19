d7e9577a0f1842a190b0eaa0c2aa7b82312c8da8
Snehal Chitnavis <ssc3322@g.rit.edu>
2018-04-19 14:41:02 -0400

    Add files via upload

---
+++ Debug/src/AES_test.d
@@ -0,0 +1,10 @@
+src/AES_test.d: ../src/AES_test.c \
+ ../../AES_test_bsp/ps7_cortexa9_0/include/xtime_l.h \
+ ../../AES_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../AES_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../../AES_test_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+../../AES_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../AES_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/AES.c \
+../src/AES_test.c \
+../src/platform.c 
+
+LD_SRCS += \
+../src/lscript.ld 
+
+OBJS += \
+./src/AES.o \
+./src/AES_test.o \
+./src/platform.o 
+
+C_DEPS += \
+./src/AES.d \
+./src/AES_test.d \
+./src/platform.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM gcc compiler'
+	arm-xilinx-eabi-gcc -Wall -O0 -g3 -pg -c -fmessage-length=0 -I../../AES_test_bsp/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
